f2a5821eff8b244cbaa931445c7fd6a345ee2c37
Michael Trujillo <trujillom9@gmail.com>
2020-08-03 17:10:36 -0700

    added a question array outline and js display styles to divs

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let button = document.querySelector("#startBtn")
-let container = document.querySelector(".container")
 let box = document.getElementById("box")
 let status = document.getElementById("rightWrong")
 let score = document.getElementById("score")
 let timer = document.getElementById("timer")
+let questionBox = document.getElementById("questionBox")
 
 let questionArr = [
-{ q: "The sky is blue.", a: "t" },
-{ q: "There are 365 days in a year.", a: "t" },
-{ q: "There are 42 ounces in a pound.", a: "f" },
-{ q: "The Declaration of Independence was created in 1745.", a: "f" },
-{ q: "Bananas are vegetables.", a: "f" }]
+{ q0: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q1: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q2: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q3: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q4: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+]
+
+// questionBox.style.display === none
+questionBox.style.display = "none"
 
 button.addEventListener("click", function(event){
 
-    score.innerHTML = 0;
+    questionBox.style.display = "block"
+    score.innerHTML = "Score: " + 0;
+    box.innerHTML = "";
 
     for (let i = 0; i < questionArr.length; i++) {
-        box.innerHTML = "";
-        box.innerHTML = JSON.stringify(questionArr[i]);
-        if (q === a) {
+        
+
+        if (q[i] === a) {
             score++
             status.innerHTML = "Correct!"
             
