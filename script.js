c481f33a38b631b75f06f1c83f82c0b825ac80bb
Michael Trujillo <trujillom9@gmail.com>
2020-08-03 17:41:09 -0700

    added more buttons in html and linked array to buttons

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ let status = document.getElementById("rightWrong")
 let score = document.getElementById("score")
 let timer = document.getElementById("timer")
 let questionBox = document.getElementById("questionBox")
+let question = document.getElementById("question")
+let option1 = document.getElementById("option1")
+let option2 = document.getElementById("option2")
+let option3 = document.getElementById("option3")
+let option4 = document.getElementById("option4")
 
 let questionArr = [
-{ q0: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
-{ q1: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
-{ q2: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
-{ q3: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
-{ q4: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
+{ q: "wut", o1: "1", o2: "2", o3: "3", o4: "4" },
 ]
 
-// questionBox.style.display === none
 questionBox.style.display = "none"
+
 
 button.addEventListener("click", function(event){
 
@@ -23,19 +28,19 @@ button.addEventListener("click", function(event){
     box.innerHTML = "";
 
     for (let i = 0; i < questionArr.length; i++) {
-        
 
-        if (q[i] === a) {
-            score++
-            status.innerHTML = "Correct!"
-            
-        } else {
-            timer--
-            status.innerHTML = "Wrong!"
-            
-        };
+        question.innerHTML = questionArr[i].q;
+        option1.innerHTML = questionArr[i].o1;
+        option2.innerHTML = questionArr[i].o2;
+        option3.innerHTML = questionArr[i].o3;
+        option4.innerHTML = questionArr[i].o4;
 
-        
+        // if (q[i] === a) {
+        //     score++
+        //     status.innerHTML = "Correct!"            
+        // } else {
+        //     timer--
+        //     status.innerHTML = "Wrong!"          
+        // };        
     };
-
 })
